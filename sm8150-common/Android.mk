da0e331f4a0bf3891683ba94e1daac04e9c519b8
LuK1337 <priv.luk@gmail.com>
2019-10-22 23:35:42 +0200

    sm8150-common: Build more stuff from source
    Change-Id: Id5893969ced857446f669920f85658094d596cae

--- sm8150-common/Android.mk
+++ sm8150-common/Android.mk
@@ -210,7 +210,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := TimeService
 LOCAL_MODULE_OWNER := oneplus
 LOCAL_SRC_FILES := proprietary/vendor/app/TimeService/TimeService.apk
-LOCAL_CERTIFICATE := platform
+LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := APPS
 LOCAL_DEX_PREOPT := false
